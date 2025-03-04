50737be90fe28765d183f073f61203819f66615f
SanjaStoj <92270917+SanjaStoj@users.noreply.github.com>
2025-03-04 11:08:04 +0000

    Update Main cvlass

--- src/main/java/setup/Main.java
+++ src/main/java/setup/Main.java
@@ -8,12 +8,19 @@ import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
 
 public class Main {
 
+  static protected WebDriver driver;
 
   static {
+     chromedriver().setup();
+     var options = new ChromeOptions();
+     options.addArguments("--no-sandbox");// bz we are using codespace container, this prevents from complications
+     driver = new ChromeDriver(options); //new chrome browser will be opened
 
   }
 
   public static void main(String[] args) {
+    driver.get("https://google.com");
+    driver.quit();
 
   }
 }
